a928f485e35c9a12a17897c84092e7be1ce02c3b
Michael Padiernos <m.padiernos@outlook.com>
2019-10-30 21:59:16 -0400

    addition: updated sources for images

--- js/index.js
+++ js/index.js
@@ -39,4 +39,16 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let cta = document.getElementById("cta-img");
+cta.setAttribute('src', siteContent['cta']['img-src']);
+
+let midImg = document.getElementById('middle-img');
+midImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+let contactHead = document.querySelector('.contact h4');
+contactHead.textContent = siteContent['contact']['contact-h4'];
+
+let footer = document.querySelector('footer p');
+footer.textContent = siteContent['footer']['copyright'];
